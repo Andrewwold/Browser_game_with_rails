8382caf039934a35587ebd09d8eb8742f47b8c0a
Andrew <goldsabre@gmail.com>
2017-09-07 15:32:37 -0600

    working on JS grid system

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,58 @@
 //= require popper
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+ $(document).ready(function() {
+
+ $('#form_id').hide(); //Initially form wil be hidden.
+
+  $('#button_id').click(function() {
+   $('#form_id').show();//Form shows on button click
+
+   });
+ });
+
+(function(name,data){
+ if(typeof onTileMapLoaded === 'undefined') {
+  if(typeof TileMaps === 'undefined') TileMaps = {};
+  TileMaps[name] = data;
+ } else {
+  onTileMapLoaded(name,data);
+ }
+ if(typeof module === 'object' && module && module.exports) {
+  module.exports = data;
+ }})("try4",
+{ "height":10,
+ "hexsidelength":0,
+ "layers":[
+        {
+         "data":"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAQAAAAEAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAABAAAAAQAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAEAAAABAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==",
+         "encoding":"base64",
+         "height":10,
+         "name":"Tile Layer 1",
+         "opacity":1,
+         "type":"tilelayer",
+         "visible":true,
+         "width":10,
+         "x":0,
+         "y":0
+        }],
+ "nextobjectid":1,
+ "orientation":"hexagonal",
+ "renderorder":"right-down",
+ "staggeraxis":"y",
+ "staggerindex":"odd",
+ "tiledversion":"1.0.3",
+ "tileheight":80,
+ "tilesets":[
+        {
+         "firstgid":1,
+         "source":"eh.tsx"
+        }],
+ "tilewidth":80,
+ "type":"map",
+ "version":1,
+ "width":10
+});
+ 
