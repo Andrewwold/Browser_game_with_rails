098d922fd2aba610081d510cfc83d85a8eefcafc
Andrew <goldsabre@gmail.com>
2017-09-20 09:14:13 -0600

    added status to users, made an enum to assign player status to users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  enum status: { standard: 0, player1: 1, player2: 2 }
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
